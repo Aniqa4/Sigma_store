84ea0704561011a57ace25a85c0cde9281723a00
Aniqa <aanika.chowdhury04@gmail.com>
2024-04-03 21:02:25 +0600

    designed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,18 @@ import { TiMail } from "react-icons/ti";
 import { VscLocation } from "react-icons/vsc";
 import { FaFacebookF, FaTwitter, FaInstagram, FaVimeoV } from "react-icons/fa";
 import { FaArrowDownLong } from "react-icons/fa6";
+import { HiMiniPlusSmall } from "react-icons/hi2";
 
 function Navbar() {
   return (
     <div>
+      {/*------first layer-----*/}
       <div className='bg-[#262626] px-5 py-3 flex justify-between'>
         <p className='text-2xl font-semibold text-white'>evanto
           <span className='text-[#6f9a37] font-normal'>market</span></p>
         <button className='text-white px-5 py-1 bg-[#6f9a37] rounded text-sm'>Buy now</button>
       </div>
+      {/*------second layer-----*/}
       <div className=' py-2 bg-[#0B2B3C] border-t border-black'>
         <div className='container mx-auto flex justify-between text-white'>
           <div className='flex gap-5'>
@@ -26,7 +29,19 @@ function Navbar() {
             <FaInstagram />
             <FaVimeoV />
             <p className='ps-5 flex items-center gap-1'>English
-            <span className=' text-[#6f9a37] text-sm '><FaArrowDownLong /></span></p>
+              <span className=' text-[#6f9a37] text-sm '><FaArrowDownLong /></span></p>
+          </div>
+        </div>
+      </div>
+      {/*------3rd layer-----*/}
+      <div className='bg-[#FFFFFF] py-8'>
+        <div className=' container mx-auto flex justify-between'>
+          <div className='flex gap-28 items-center'>
+            <img src="/FoodStore.png" alt="logo" />
+            <ul className='flex gap-5 font-semibold'>
+              <li className='flex items-center'>Home <span className='text-xl'><HiMiniPlusSmall /></span></li>
+              <li className='flex items-center'>Shop <span className='text-xl'><HiMiniPlusSmall /></span></li>
+            </ul>
           </div>
         </div>
       </div>
