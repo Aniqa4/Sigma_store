70407da10e0f8b96bd92adb74353a6038416e701
Aniqa <aanika.chowdhury04@gmail.com>
2024-04-03 20:45:36 +0600

    completed second navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { TiMail } from "react-icons/ti";
+import { VscLocation } from "react-icons/vsc";
+import { FaFacebookF, FaTwitter, FaInstagram, FaVimeoV } from "react-icons/fa";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 function Navbar() {
   return (
@@ -8,8 +12,23 @@ function Navbar() {
           <span className='text-[#6f9a37] font-normal'>market</span></p>
         <button className='text-white px-5 py-1 bg-[#6f9a37] rounded text-sm'>Buy now</button>
       </div>
-      <div className='container mx-auto py-3'>
-        <div></div>
+      <div className=' py-2 bg-[#0B2B3C] border-t border-black'>
+        <div className='container mx-auto flex justify-between text-white'>
+          <div className='flex gap-5'>
+            <p className='flex items-center'>
+              <span className='text-[#6f9a37] text-xl'><TiMail /></span>info12@example.com</p>
+            <p className=' flex items-center'>
+              <span className='text-[#6f9a37] text-xl'  ><VscLocation /></span>California, TX 70240</p>
+          </div>
+          <div className='flex items-center gap-5'>
+            <FaFacebookF />
+            <FaTwitter />
+            <FaInstagram />
+            <FaVimeoV />
+            <p className='ps-5 flex items-center gap-1'>English
+            <span className=' text-[#6f9a37] text-sm '><FaArrowDownLong /></span></p>
+          </div>
+        </div>
       </div>
     </div>
   )
