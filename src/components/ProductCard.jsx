d7a8bfa588848ea2657543df53aec49fb3e3ad00
Aniqa <aanika.chowdhury04@gmail.com>
2024-04-04 18:08:47 +0600

    completed card design

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
+import { MdFavoriteBorder } from "react-icons/md";
 import { FaStar } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
 import '../styles/cardStyle.css'
 
 function ProductCard() {
     return (
-        <div className='bg-[#FFFFFF] mx-auto p-5 border hover:border-[#7BAE00] rotate-image'>
-            <img src="/product.png" alt=""/>
-            <div className='text-center grid gap-3'>
-                <div className='flex justify-center'>
-                    <p className='flex gap-1 text-[#FC0]'><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+        <div className='bg-[#FFFFFF] mx-auto p-5 border hover:border-[#7BAE00] relative rotate-image  add-to-cart-effect'>
+            <div className='grid gap-5'>
+                <div className='relative'>
+                    <img src="/product.png" alt="" />
+                    <div className='add-to-cart'>
+                        <div className='flex justify-center'>
+                            <p className='hover:bg-[#7BAE00] bg-white font-semibold shadow-lg px-10 py-2 '
+                                style={{ boxShadow: '50px 0 10px rgba(0, 0, 0, 0.1)' }}>Add to Cart</p>
+                        </div>
+                    </div>
                 </div>
-                <p className=' font-semibold'>Product Name</p>
-                <p className='flex items-center mx-auto'>
-                    <span className='text-[#7BAE00] font-extrabold text-xl'>$200</span>&nbsp;
-                    <span className=' line-through'>$210</span>
-                </p>
+                <div className='text-center grid gap-3'>
+                    <div className='flex justify-center'>
+                        <p className='flex gap-1 text-[#FC0]'><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                    </div>
+                    <p className=' font-semibold'>Product Name</p>
+                    <p className='flex items-center mx-auto'>
+                        <span className='text-[#7BAE00] font-extrabold text-xl'>$200</span>&nbsp;
+                        <span className=' line-through'>$210</span>
+                    </p>
+                </div>
+            </div>
+            <div className=' absolute top-0 left-0'>
+                <p className='bg-[#7BAE00] font-bold text-white uppercase px-5 py-1'>Sale</p>
+            </div>
+            <div className=' absolute top-3 right-3'>
+                <p className='bg-[#7BAE00] font-bold text-white text-xl uppercase p-3 rounded-full'><MdFavoriteBorder /></p>
+            </div>
+            <div className=' absolute top-16 right-3 view-button'>
+                <p className='bg-[#7BAE00] font-bold text-white text-xl uppercase p-3 rounded-full'><FaRegEye /></p>
             </div>
         </div>
     )
