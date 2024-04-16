cd17fd9db2a9f349f66ea08b92125e364150ac64
Aniqa <aanika.chowdhury04@gmail.com>
2024-04-17 02:31:54 +0600

    completed shop banner

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BsDot } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 function Shop() {
   return (
-    <div>Shop</div>
+    <div>
+      <div className='relative'>
+        <img src="/shop.jpg" alt="" className='w-full' />
+        <div className=' bg-[#000000bf] absolute top-0 left-0 bottom-0 right-0 '>
+          <div className='max-w-[65%] mx-auto text-white flex items-center h-full '>
+            <div className='flex justify-between items-center w-full'>
+              <p className=' text-4xl font-bold'>Shop</p>
+              <div className='flex gap-2 items-center font-semibold'>
+                <Link to={'/'}>Home</Link>
+                <span className=' text-xl'><BsDot /></span>
+                <p className='text-[#666]'>Shop</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
